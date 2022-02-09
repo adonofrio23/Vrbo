aa18a1ff0b90ff7411305a568a1553f9dd71de01
adonofrio10 <70344654+adonofrio10@users.noreply.github.com>
2022-02-09 17:09:43 -0500

    Hi

--- Vrbo Software Engineering Project/src/vrboDemo/vrboDemo.java
+++ Vrbo Software Engineering Project/src/vrboDemo/vrboDemo.java
@@ -7,5 +7,6 @@ public class vrboDemo {
 		System.out.println("Prices of the two cars in question:");	
 		System.out.println("Prices of the two cars in question:");	
 		System.out.println("Prices of the two cars in question:");	
+		System.out.println("Prices of the two cars in question:");	
 	}
 }
