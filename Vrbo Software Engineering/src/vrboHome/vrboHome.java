961a2b402aeed8b3094f0fab5bf31070811f708f
travistaylor <travistaylor@localhost>
2022-02-10 19:35:21 -0500

    Slogan + Logo

--- Vrbo Software Engineering/src/vrboHome/vrboHome.java
+++ Vrbo Software Engineering/src/vrboHome/vrboHome.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
@@ -67,16 +68,28 @@ public class vrboHome extends JFrame{
 			setContentPane(contentPane);
 			
 			//Travis - Header with Login, Sign up, List your Property, and Exit Buttons
-			JButton exitButton = createButton("Exit", 1070, 15, 80, 80);
+			JButton exitButton = createButton("Exit", 16, 1070, 15, 80, 80);
 			exitButton.addActionListener(new ExitListener()); // Pop-Up Window Functionality
-			JButton listingButton = createButton("List Your Property", 870, 15, 150, 80);
-			JButton signUpButton = createButton("Sign Up", 700, 15, 120, 80);
-			JButton logInButton = createButton("Log In", 530, 15, 120, 80);
+			JButton listingButton = createButton("List Your Property", 16, 870, 15, 150, 80);
+			JButton signUpButton = createButton("Sign Up", 16, 700, 15, 120, 80);
+			JButton logInButton = createButton("Log In", 16, 530, 15, 120, 80);
 			
 			contentPane.add(exitButton);
 			contentPane.add(listingButton);
 			contentPane.add(signUpButton);
 			contentPane.add(logInButton);
+			
+			JLabel slogan = createLabel("  Travel Better Together", 20, 250, 15, 230, 80);
+			slogan.setBorder(BorderFactory.createLineBorder(Color.black));
+			
+			contentPane.add(slogan);
+			
+			JLabel logo = new JLabel();
+			Image vrboLogo = fetchImage("https://images.ctfassets.net/uylld2rxwr0n/7H3jIk9OvCrmuFpSyUrkqq/b07aa3c9796d08f2773bc54c69c666d9/Vrbo_logo_dark.png?w=960&q=50", 150, 80);
+			logo.setIcon(new ImageIcon(vrboLogo));
+			logo.setBounds(50, 15, 150, 80);
+			
+			contentPane.add(logo);
 			
 			//Juliette - Search bar
 			
@@ -197,11 +210,30 @@ public class vrboHome extends JFrame{
 	 		}
 	 	}
 	 
-	 	private JButton createButton(String tag, int x, int y, int width, int height) {
+	 	private JButton createButton(String tag, int fontSize, int x, int y, int width, int height) {
 	 		JButton btn = new JButton(tag);
-			btn.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			btn.setFont(new Font("Tahoma", Font.PLAIN, fontSize));
 			btn.setBounds(x, y, width, height);
 			return btn;
+	 	}
+	 	
+	 	private JLabel createLabel(String tag, int fontSize, int x, int y, int width, int height) {
+	 		JLabel label = new JLabel(tag);
+	 		label.setFont(new Font("Tahoma", Font.PLAIN, fontSize));
+	 		label.setBounds(x, y, width, height);
+	 		return label;
+	 	}
+	 	
+	 	private Image fetchImage(String url, int width, int height) {
+	 		Image image = null;
+	 		try {
+	 			URL u = new URL(url);
+	 			BufferedImage img = ImageIO.read(u);
+	 			image = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+	 		} catch (Exception e) {
+	 
+	 		}
+	 		return image;
 	 	}
 	 	
 	    //
