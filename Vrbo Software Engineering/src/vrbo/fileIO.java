76f4438712b0438ebad67cd54faf3c1f86fdb393
adonofrio10 <70344654+adonofrio10@users.noreply.github.com>
2022-02-16 18:25:42 -0500

    FileIO added

---
+++ Vrbo Software Engineering/src/vrbo/fileIO.java
@@ -0,0 +1,53 @@
+package vrbo;
+
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class fileIO 
+{
+		String fileName = null;
+		
+		public fileIO(String fn)
+		{
+			fileName = fn;
+		}
+		
+		public void wrSignUpData(String dataStr)
+		{
+	        FileWriter fwg = null;
+	        try 
+	        {
+	        	// open the file in append write mode
+	        	fwg = new FileWriter(fileName, true);
+	        }
+	        catch (IOException e)
+	        {
+	        	// TODO Auto-generated catch block
+	        	e.printStackTrace();
+	        }
+	   	    
+	        BufferedWriter bwg = new BufferedWriter(fwg);
+	        PrintWriter outg   = new PrintWriter(bwg);
+			
+	        String timeStamp = new SimpleDateFormat("MM-dd-yyyy HH.mm.ss").format(new Date());
+	        
+	        outg.println(timeStamp + " : " + dataStr);
+	        
+	        outg.close();
+		}
+		
+		public void readUserPass(String username, String password)
+				throws IOException 
+				{    
+				    BufferedReader reader = new BufferedReader(new FileReader(fileName));
+				    String currentLine = reader.readLine();
+				    // Add for loop with if statements seeing if username = current line and password = current line
+				    reader.close();
+				}
+}
