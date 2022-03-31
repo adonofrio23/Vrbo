30dbe7c80b82df2c3d6c8042509a152e5c595cf8
travistaylor <travistaylor@localhost>
2022-03-31 14:13:53 -0400

    Offline Mode Prompts

--- Vrbo Software Engineering/src/app/Window.java
+++ Vrbo Software Engineering/src/app/Window.java
@@ -15,47 +15,74 @@ public class Window extends JFrame {
 	public static void initializeWindow(int width, int height, String name) {
 		try {
 			frame = new JFrame();
-			frame.setSize(width, height);
-			frame.setLocationRelativeTo(null);
-			
-			frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-			frame.addWindowListener(new java.awt.event.WindowAdapter() {
-				@Override public void windowClosing(java.awt.event.WindowEvent e) {
-					e.getWindow().dispose();
-				}
-				@Override public void windowClosed(java.awt.event.WindowEvent e) {
-					e.getWindow().dispose();
-					System.out.println("[System] - VRBO App Terminated");
-					
-					if (SocketUtils.socketConnected) {
-						SocketUtils.Disconnect();
-						System.out.println("[Socket] - Disconnected From Socket");
-					}
-					
-					System.exit(0);
-				}
-			});
 			System.out.println("[JFrame] - Window Initialized Successfully");
 		} catch (HeadlessException e) {
 			System.out.println("[JFrame] - Window Failed To Initialize");
 			System.exit(1);
 		}
 		
-		System.out.println("[Socket] - Trying To Connect To \"" + SocketUtils.ip + ":" + SocketUtils.port + "\"");
-		if (!SocketUtils.Connect()) {
-			System.out.println("[Socket] - Failed To Connect To \"" + SocketUtils.ip + ":" + SocketUtils.port + "\"");
-			frame.dispatchEvent(new java.awt.event.WindowEvent(frame, java.awt.event.WindowEvent.WINDOW_CLOSED));
-		}
+		frame.setSize(width, height);
+		frame.setLocationRelativeTo(null);
 		
-		System.out.println("[Socket] - Connected To " + SocketUtils.ip + ":" + SocketUtils.port);
-		SocketUtils.socketConnected = true;
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override public void windowClosing(java.awt.event.WindowEvent e) {
+				e.getWindow().dispose();
+			}
+			@Override public void windowClosed(java.awt.event.WindowEvent e) {
+				e.getWindow().dispose();
+				System.out.println("[System] - VRBO App Terminated");
+				
+				if (SocketUtils.socketConnected) {
+					SocketUtils.Disconnect();
+					System.out.println("[Socket] - Disconnected From Socket");
+				}
+				
+				System.exit(0);
+			}
+		});
+		frame.setVisible(true);
+		
+		int offline = JOptionPane.showConfirmDialog(
+					null, 
+					"Start In Offline Mode?", // Exit message
+					"VRBO Client",  // Pop-Up Window title
+					JOptionPane.INFORMATION_MESSAGE // Options
+			);
+			
+			if (offline != JOptionPane.OK_OPTION) {
+				frame.setTitle("Trying To Connect To Server...");
+				System.out.println("[Socket] - Trying To Connect To \"" + SocketUtils.ip + ":" + SocketUtils.port + "\"");
+				if (!SocketUtils.Connect()) {
+					System.out.println("[Socket] - Failed To Connect To \"" + SocketUtils.ip + ":" + SocketUtils.port + "\"");
+					
+					int res = JOptionPane.showConfirmDialog(
+		 					null, 
+		 					"Failed To Connect To Server. Start In Offline Mode?", // Exit message
+		 					"VRBO Client",  // Pop-Up Window title
+		 					JOptionPane.INFORMATION_MESSAGE // Options
+		 			);
+		 			
+		 			if (res != JOptionPane.OK_OPTION) {
+		 				frame.dispatchEvent(new java.awt.event.WindowEvent(frame, java.awt.event.WindowEvent.WINDOW_CLOSED));
+		 			}
+					System.out.println("[Socket] - Starting VRBO Application In Offline Mode");
+					name += " OFFLINE";
+				} else {
+					System.out.println("[Socket] - Connected To " + SocketUtils.ip + ":" + SocketUtils.port);
+					SocketUtils.socketConnected = true;
+				}
+			} else if (offline == JOptionPane.OK_OPTION) {
+				System.out.println("[Socket] - Starting VRBO Application In Offline Mode");
+				name += " OFFLINE";
+			}
 		
 		new Home();
 		ViewManager.initializeView();
 		frame.add(ViewManager.Container);
 		System.out.println("[JPanel] - Home Page Loaded Successfully");
 		
-		frame.setVisible(true);
+		frame.revalidate();
 		
 		initializeClock(name);
 	}
