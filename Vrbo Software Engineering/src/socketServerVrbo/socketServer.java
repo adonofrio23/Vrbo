711afeacc7c1ae377d6e74fddbe68ec4b2389cc2
Juliette Garvey <jmgarvey01@gmail.com>
2022-04-06 18:46:04 -0400

    Send message to server with message through login

--- Vrbo Software Engineering/src/socketServerVrbo/socketServer.java
+++ Vrbo Software Engineering/src/socketServerVrbo/socketServer.java
@@ -23,11 +23,14 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Vector;
+
+import app.fileIO;
+
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 
-public class socketServer {	
+public class socketServer implements Runnable {	
 	static int port = 4545;
 	static final String newline = "\n";
 	static int first_time = 1;
@@ -36,16 +39,19 @@ public class socketServer {
 	static int numOfMessages = 0;
 	static int max_connections = 5;
 	static int numOfTransactions = 0;
-	
+	static String ipString;
+	Socket csocket;
 	static boolean found = false;
 	static String ipAddrOfSocketServer = null;
 
-//	socketServer(Socket csocket, String ip)
-//	{
-//		this.csocket  = csocket;
-//		this.ipString = ip;
-//	} 
-//	
+
+	socketServer(Socket csocket, String ip)
+	{
+		this.csocket  = csocket;
+		socketServer.ipString = ip;
+	} 
+	
+	
 	static void displayInterfaceInformation(NetworkInterface netint) throws SocketException 
     {	
     	String dname = netint.getName();
@@ -73,6 +79,8 @@ public class socketServer {
 	
 	
 	public static void runSockServer() {
+		
+		boolean sessionDone = false;	
 		
 		try {
 			ssock = new ServerSocket(port);
@@ -122,20 +130,189 @@ public class socketServer {
 		     
 	     socketServerVrbo.bottomQuadL.setText(ipAddrOfSocketServer + newline);
 	     socketServerVrbo.bottomQuadL.append("Port: " + Integer.toString(port));
+		 socketServerVrbo.bottomQuadL.append("Listening on port " + port + newline);
+
+	     
+	     sessionDone = false;
+	     while (sessionDone == false)
+	     {
+	        Socket sock = null;
+		    try
+		    {
+		       //	
+  	     	   // blocking system call
+		       //	
+			   sock = ssock.accept();
+		    }
+		    catch (IOException e)
+		    {
+			   e.printStackTrace();
+		    }
+		 
+	        socketServerVrbo.bottomQuadL.append("Client Connected : " + sock.getInetAddress()+ newline);
+	        new Thread(new socketServer(sock, sock.getInetAddress().toString())).start();
+	        
+	     }
+	     
+	     endSocketServer();
+	}
+	     
+	 public static void endSocketServer(){
+	     try 
+	     {
+		    ssock.close();
+	     }
+	     catch (IOException e) 
+	     {
+		    e.printStackTrace();
+	     }
 	}
 	 
-	  //   socketServerVrbo.wrToCenterTA("Listening on port " + port_num + newline);
-	 
-	     //
-	     // initialize the hash table to the following keys or if file hash table data exists, then use it
-	     //
+	   
+
+	public void run()
+	{
+	   long threadId = 0;
+	   
+	   try
+	   {
+		  int watchDog = 0;
+		  boolean session_done = false;
+	      String clientString;
 	    
-	
-	public static void endSocketServer() {
-		try {
-			ssock.close();
-		} catch (IOException e) {
+	      threadId = Thread.currentThread().getId();
+	      
+	      numOfConnections++;
+	      
+	      socketServerVrbo.bottomQuadL.append("Num of Connections = " + numOfConnections + newline);
+	      
+	   
+	      //
+	      // write IP address of the client who just connected to the main screen
+	      //
+	 
+	       
+	      PrintStream pstream = new PrintStream (csocket.getOutputStream());
+	      BufferedReader rstream = new BufferedReader(new InputStreamReader(csocket.getInputStream()));
+	     
+	      while (session_done == false)
+	      {
+	          if (rstream.ready())   // check for any data messages
+	          {
+	              clientString = rstream.readLine();
+
+	              watchDog = 0;             // reset the watch counter back to zero
+	              
+	              //
+	              // write to transaction log
+	              //
+	            
+	              	              
+	              // update the status text area to show progress of program
+	   	           socketServerVrbo.bottomQuadL.append("RECV : " + clientString + newline);
+	     	       
+	     	       // update the status text area to show progress of program
+	     	       socketServerVrbo.bottomQuadL.append("RLEN : " + clientString.length() + newline);
+	              
+	              if (clientString.length() > 128)
+	              {
+	           	   session_done = true;
+	           	   continue;
+	              }
+
+	              if (clientString.contains("quit"))
+	              {
+	                 session_done = true;
+	              }
+	              else if (clientString.contains("QUIT"))
+	              {
+	                 session_done = true;
+	              }
+	              else if (clientString.contains("Quit"))
+	              {
+	                 session_done = true;
+	              }
+	             
+	              else
+	              {
+	            	  pstream.println("NACK : ERROR : No such command!");
+	              }
+	       	   }
+	         			    		        	
+	           Thread.sleep(500);
+
+	           if (++watchDog >= 40)     // if not messages by this client in 40 seconds, then close connection
+	           {
+	        	   watchDog = 0;
+	        	   session_done = true;
+	           }
+	        }    // end WHILE LOOP - session_done
+	      
+	      
+	       
+	      
+	        numOfConnections--;
+
+	        // close client socket
+	        csocket.close();
+	       
+	        // update the status text area to show progress of program
+		   
+		     socketServerVrbo.bottomQuadL.append("Num of Connections = " + numOfConnections + newline);
+		     
+		     return;
+	     } // end try  
+	 
+	     catch (SocketException e)
+	     {
+		  // update the status text area to show progress of program
+	      socketServerVrbo.bottomQuadL.append("ERROR : Socket Exception!" + newline);
+	     }
+	     catch (InterruptedException e)
+	     {
+		  // update the status text area to show progress of program
+	      socketServerVrbo.bottomQuadL.append("ERROR : Interrupted Exception!" + newline);
+	     }
+	     catch (UnknownHostException e)
+	     {
+		  // update the status text area to show progress of program
+	      socketServerVrbo.bottomQuadL.append("ERROR : Unkonw Host Exception" + newline);
+	     }
+	     catch (IOException e) 
+	     {
+	     // update the status text area to show progress of program
+	      socketServerVrbo.bottomQuadL.append("ERROR : IO Exception!" + newline);
+	     }     
+	     catch (Exception e)
+	     { 
+		  numOfConnections--;
+		  
+		  // update the status text area to show progress of program
+	      socketServerVrbo.bottomQuadL.append("ERROR : Generic Exception!" + newline);
+	     }
+	   
+         
+        
+         if (numOfConnections > 0)
+            numOfConnections--;
+
+         // close client socket
+         try 
+         { 
+			csocket.close();
+		 } 
+         catch (IOException e)
+         {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	}
+		 }
+       
+         // update the status text area to show progress of program
+	     socketServerVrbo.bottomQuadL.append("Num of Connections = " + numOfConnections + newline);
+	     
+	  }
 }
+// end run() thread method
+	
+	
+
