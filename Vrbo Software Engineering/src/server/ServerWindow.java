efc2fb25ba5047dd7b180fb8ce772e42559c99d4
adonofrio10 <70344654+adonofrio10@users.noreply.github.com>
2022-05-04 14:13:36 -0400

    Added scroll to messages

--- Vrbo Software Engineering/src/server/ServerWindow.java
+++ Vrbo Software Engineering/src/server/ServerWindow.java
@@ -1,6 +1,8 @@
 package server;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -11,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
@@ -74,6 +77,7 @@ public class ServerWindow {
 		bottomQuadL.setBackground(Color.WHITE);
 		ServerWindow.add(bottomQuadL);
 		
+		
 		//
 		//Bottom Quadrant Right - Users
 		//
@@ -83,8 +87,7 @@ public class ServerWindow {
 		bottomQuadR.setBorder(new EtchedBorder(EtchedBorder.RAISED));
 		bottomQuadR.setBackground(Color.WHITE);
 		ServerWindow.add(bottomQuadR);
-		
-		
+
 		//
 		// BOTTOM - available text area
 		//
